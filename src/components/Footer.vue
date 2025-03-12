9f456a7abc9b9c2d42a661003be7a3b1e1ed2251
NguyenHoangTuan <nguyenhoangtuan05052001@gmail.com>
2025-03-12 13:44:30 +0700

    second

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="mt-8 border z-10 border-t-[#33353F] border-1-transparent border-r-transparent text-white">
     <div class="container p-12 flex justify-between ">
-      <div class="text-white text-3xl font-bold">LOGO</div>
+      <div class="text-white text-3xl font-bold">LOGO aoo</div>
       <p class="text-slate-600">All rights reserved.</p>
     </div>
   </footer>
